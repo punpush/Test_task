7458e0f9b73037f3af3e88a0cd953ce05b2fc9bc
punpush <denissskrasilnikov00@gmail.com>
2022-12-13 20:26:13 +0300

    start

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,9 +10,16 @@
 #include<condition_variable>
 
 
-int main()
+struct Message
 {
-    setlocale(LC_ALL, "ru"); // Поддержка русского языка
+    Message(size_t file_size, int* data_);
+
+    size_t file_size;
+    int* data_;
+};
+
+
+void read() {
     int T = 0;
     int M = 0;
     std::cout << "Введите периодичность заполнения T = "; // Ввод неоюходимых параметров
@@ -21,5 +28,22 @@ int main()
     std::cout << "Введите максимальный объем записываеых данных M = ";
     std::cin >> M;
     std::cout << std::endl;
+}
+
+void create() {
+    ;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru"); // Поддержка русского языка
+    
+    std::queue<Message> message_queue;
+
+    std::thread th1([&]() {read(); });
+    std::thread th2([&]() {create(); });
+    th1.join();
+    th2.join();
 }
 
