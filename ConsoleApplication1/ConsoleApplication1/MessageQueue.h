98e3efa96c41b44e39ca57d24ad734092238a4fe
punpush <denissskrasilnikov00@gmail.com>
2022-12-18 19:35:22 +0300

    not finished

--- ConsoleApplication1/ConsoleApplication1/MessageQueue.h
+++ ConsoleApplication1/ConsoleApplication1/MessageQueue.h
@@ -10,30 +10,28 @@
 // Класс сообщений, содержащий размер и указатель на данные в буфере
 
 template<typename Type>
-struct MessageQueue : Buffer<Type> // Наследование от буфера
+struct MessageQueue  
 {
 
-    MessageQueue() {};
+    MessageQueue(size_t size_) : size_(size_) {};
     
 
 
-     // Функция записи сообщений 
-    void read() {
+    void read(size_of_message, data_) {
 
-        
-        Type* data_ = Buffer<Type>::get(); // Получение адреса хвоста
-        size_t size = sizeof(this->tail);  // Размер хвоста
-
-        gq.push({size,data_}); // очередь, хранящая пару из размера и указателя на область памяти
-
-        size_ ++ // увеличение размера очереди 
-
+        if (size_now < size_) {
+            gq.push({ size_of_message,data_ }); // очередь, хранящая пару из размера и указателя на область памяти
+            size_now++; // увеличение размера очереди 
+        }
     }
 
 
 protected:
-    Type* data_ = 0;
-    size_t size = 0;
-    size_t size_ = 0;
-    std::queue<std::pair<size_t, Type*>> gq;
+
+    Type* data_;
+    Buffer<Type>& buffer;
+    size_t size_of_message = 0; // размер читаемых данных
+    size_t size_now = 0; // текущая очередь
+    size_t size_ = 0; // дазмер очереди
+    std::queue<std::pair<size_t, Type*>> gq; // очередь сообщений 
 };
