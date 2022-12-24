43c68d8524c9bf15277faa33ace5ff667b6093d0
punpush <118619904+punpush@users.noreply.github.com>
2022-12-24 22:01:37 +0300

    complete

--- ConsoleApplication/ConsoleApplication/MessageQueue.h
+++ ConsoleApplication/ConsoleApplication/MessageQueue.h
@@ -15,30 +15,31 @@ struct MessageQueue
 
     MessageQueue(size_t size_) : size_(size_) {}
 
-
+    // Метод записи сообщения в очередь 
     void get_message(std::pair<size_t, Type*> message) {
 
-        std::unique_lock<std::mutex> ul1(data_mutex);
 
         if (size_now < size_) {
+
             size_data.push(message); // очередь, хранящая пару из размера и указателя на область памяти
-            size_now++; // увеличение размера очереди
+
+            size_now++; // увеличение размера очереди 
+
 
         }
         else throw std::runtime_error("queue is full");
 
         cv.notify_one();
 
-        ul1.unlock();
     }
 
 
-
+    // Метод отправки сообщения из очереди
     std::pair<size_t, Type*> send_message() {
 
         std::unique_lock<std::mutex> ul2(data_mutex);
 
-        while (size_data.size() == 0)
+        while (size_data.size() == 0) // Ожидание появления сообщения
         {
             cv.wait(ul2);
         }
@@ -47,12 +48,53 @@ struct MessageQueue
 
         size_data.pop();
 
+        size_now --;
+
+      
+
+        std::cout << "размер очереди " << size_now << std::endl; // Вывод размера очереди
+
         return message;
+
+        
 
         ul2.unlock();
 
     }
 
+    // Метод pop
+    void pop() {
+        if (size_data.size() == 0)
+        {
+            throw std::runtime_error("queue is empty");
+        }
+
+        size_data.pop();
+    }
+
+    // Метод push
+    void push(std::pair<size_t, Type*> n)
+    {
+        size_data.push(n);
+
+    }
+
+    // Метод получения размера для работы цикла записи
+    size_t get()
+    {
+        std::unique_lock<std::mutex> ul3(data_mutex);
+
+        while (size_data.size() == 0)
+        {
+            cv.wait(ul3);
+        }
+
+        std::pair<size_t, Type*> size = size_data.front();
+
+        return size.first;
+
+        ul3.unlock();
+    }
 
 
 public:
